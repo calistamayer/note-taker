a0cc93d11384075200d1301e48d93415059e5424
Calista Mayer <calista.a.mayer@gmail.com>
2020-11-29 13:00:13 -0800

    index html route

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const PORT = process.env.PORT || 3002;
 const app = express();
 const { notes } = require('./db/db.json');
 
+// api route for notes data
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
+
+// html route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
