4a9c91a437a58faf8ecd1feaf8f09fb8fb6af8a8
Calista Mayer <calista.a.mayer@gmail.com>
2020-11-29 12:37:31 -0800

    get notes api

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const { notes } = require('./db/db.json');
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
 
 app.listen(3002, () => {
     console.log(`API server now on port 3002!`);
