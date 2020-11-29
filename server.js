ceb9ef669668ccc9631e0d04d952b6dc8e2aba94
Calista Mayer <calista.a.mayer@gmail.com>
2020-11-29 12:52:58 -0800

    updated PORT

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3002;
 const app = express();
 const { notes } = require('./db/db.json');
 
@@ -6,6 +7,6 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-app.listen(3002, () => {
-    console.log(`API server now on port 3002!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
